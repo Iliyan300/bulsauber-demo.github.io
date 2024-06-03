1b9b97cda90591dd8013129cdf14058ecb3487c2
IliyanGeorgiev <iliangeorgiev300@yahoo.com>
2024-06-03 19:17:50 +0300

    Fixing bugs and footer

--- imageGallery.js
+++ imageGallery.js
@@ -1,33 +1,14 @@
 
-const images = [
+const images = [];
+
+const numberOfImages = 60;
+
+for(let i= 35; i <= numberOfImages; i++) {
+
+images.push(`assets/pictures/workpic (${i}).jpg`);
+
+}
    
-    'assets/pictures/workpic (39).jpg',
-    'assets/pictures/workpic (40).jpg',
-    'assets/pictures/workpic (41).jpg',
-    'assets/pictures/workpic (42).jpg',
-    'assets/pictures/workpic (43).jpg',
-    'assets/pictures/workpic (44).jpg',
-    'assets/pictures/workpic (45).jpg',
-    'assets/pictures/workpic (46).jpg',
-    'assets/pictures/workpic (47).jpg',
-    'assets/pictures/workpic (48).jpg',
-    'assets/pictures/workpic (49).jpg',
-    'assets/pictures/workpic (50).jpg',
-    'assets/pictures/workpic (51).jpg',
-    'assets/pictures/workpic (52).jpg',
-    'assets/pictures/workpic (53).jpg',
-    'assets/pictures/workpic (54).jpg',
-    'assets/pictures/workpic (55).jpg',
-    'assets/pictures/workpic (56).jpg',
-    'assets/pictures/workpic (57).jpg',
-    'assets/pictures/workpic (58).jpg',
-    'assets/pictures/workpic (59).jpg',
-    'assets/pictures/workpic (60).jpg'
-
-
-];
-
-
 
 let currentIndex = 0;
 
